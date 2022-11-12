c14b769ca91043acdc825791bcc364fc9b411004
logoo03 <althj03@gmail.com>
2022-11-12 11:19:04 +0900

    added simple login

---
+++ simple_login_example/users/views.py
@@ -0,0 +1,29 @@
+import json
+
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def login(req):
+    user_data = {
+        'id': 'python',
+        'password': 'django',
+    }
+
+    if (req.method == 'GET'):
+        user_id = req.GET['id']
+        password = req.GET['pw']
+
+        if (user_id != user_data['id']):
+            return HttpResponse('Invalid Username.')
+        elif (password != user_data['password']):
+            return HttpResponse('Invalid Password.')
+        else:
+            return HttpResponse('Login Success!')
+
+    data = json.dumps(req.GET)
+    return HttpResponse(f"""This is the main login page. <br>
+                        method: {req.method} <br>
+                        data: {data}""")
+
+def login_detail(req, id):
+    return HttpResponse(f'Your user ID is \'{id}\'.')
