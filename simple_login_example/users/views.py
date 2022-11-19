ad8ba23f2374e1df1f460254efadb196077501ac
logoo03 <althj03@gmail.com>
2022-11-19 11:27:01 +0900

    7주차 진도

--- simple_login_example/users/views.py
+++ simple_login_example/users/views.py
@@ -2,6 +2,7 @@ import json
 
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.utils.datastructures import MultiValueDictKeyError
 
 def login(req):
     user_data = {
@@ -10,15 +11,23 @@ def login(req):
     }
 
     if (req.method == 'GET'):
-        user_id = req.GET['id']
-        password = req.GET['pw']
+        return render(req, 'users/login.html')
+
+    if (req.method == 'POST'):
+        user_id = req.POST.get('id')
+        password = req.POST.get('pw')
+
+        if (not user_id):
+            return HttpResponse('No input id.')
+        if (not password):
+            return HttpResponse('No input password.')
 
         if (user_id != user_data['id']):
             return HttpResponse('Invalid Username.', status=401)
         elif (password != user_data['password']):
             return HttpResponse('Invalid Password.', status=401)
         else:
-            return HttpResponse('Login Success!')
+            return HttpResponse('<h3 style="color:blue;">Login Success!</h3>')
 
     data = json.dumps(req.GET)
     return HttpResponse(f"""This is the main login page. <br>
